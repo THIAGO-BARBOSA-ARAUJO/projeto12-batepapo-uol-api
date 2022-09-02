fb5e39fc70b6d49e5641651dcdf74feadf6bc303
THIAGO-BARBOSA-ARAUJO <thiagobarbosa.araujo@outlook.com>
2022-09-02 15:44:14 -0300

    post da rota/participants feita

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 import { MongoClient } from "mongodb"
 import express from "express"
+import joi from "joi"
+import cors from "cors"
+import dayjs from "dayjs"
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 //conectando ao banco
 const mongoClient = new MongoClient("mongodb://localhost:27017")
@@ -12,17 +16,43 @@ mongoClient.connect().then(() => {
     db = mongoClient.db("teste")
 })
 
+const nameSchema = joi.object({
+            name: joi.string()
+            .empty()
+            .required() 
+}).options({ abortEarly: false })
+
+var now = dayjs()
+
+
 app.post("/participants", (req, res) => {
 	const { name } = req.body
+    
+    async function ValidaDados() {
 
-    if(name === ""){
-        res.sendStatus(422)
-        return
+        const validador = nameSchema.validate(req.body)
+    
+        if(validador.error){
+            console.log(validador.error)
+            res.sendStatus(422)
+            console.log(validador.error)
+            return
+        }
+
+        const resposta = await db.collection("usuarios").findOne(req.body)
+            
+        if(resposta){
+            res.status(409).send("Usuário já cadastrado!")
+            return
+        }
+        // inserindo usuário
+        db.collection("usuarios").insertOne({name: name, lastStatus: Date.now()});
+        db.collection("mensagens").insertOne({from: name, to: 'Todos', text: 'entra na sala...', type: 'status', time: `${now.hour()}:${now.minute()}:${now.second()}`})
+        res.status(201).send({message: "Usuário cadastrado com sucesso!"})
     }
-
-    // inserindo usuário
-	db.collection("usuarios").insertOne({name: name, lastStatus: Date.now()});
-    res.status(201).send({message: "Usuário cadastrado com sucesso!"})
+      
+    ValidaDados()
+    
 });
 
 
